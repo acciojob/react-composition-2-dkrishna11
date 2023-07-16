20793fe5b41c4206972b9be3578d11482c550e88
Krishna_D <dkrishna051997@gmail.com>
2023-07-16 22:46:03 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [prop, setProb]=useState(true)
   return (
     <div>
-        {/* Do not remove the main div */}
+        <button className="model-overlay" onClick={()=>setProb(true)}>Show Model</button>
+        <div className="model-close" onClick={()=>setProb(false)}>
+          {prop && <div>
+              <button>Close</button>
+              <p>This is the Content of the modal</p>
+            </div>}
+        </div>
     </div>
   )
 }
